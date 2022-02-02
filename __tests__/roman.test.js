0b57334585dc6a5e360d391a74be725a869beb0e
Jack Jones <jacktjones4@gmail.com>
2022-02-01 16:13:59 -0800

    WIP writing first test

---
+++ __tests__/roman.test.js
@@ -0,0 +1,30 @@
+"Goal: should return equivilent roman numerals for inputted number"
+
+userInput: 25
+expectedOutput: XXV
+347
+347%100 = 47
+math.floor(347/100) = 3
+CCC
+XCIX = 99
+1. inputted Number
+2. roman numerals + their equivalents (key pair value??) [("1", "I"), ("2", "II") ... ]
+3. function to convert between
+    M = 1000
+    C = 100 
+    L = 50 
+    X = 10
+    V = 5
+    I = 1
+8 = viii
+9 = ix
+If smaller numbers follow larger numbers, add the numbers.For example: In the Roman numeral XII, you add 10, 1, and 1, which makes 12.
+
+If a smaller number precedes a larger number, subtract the smaller number.For example: In the Roman numeral IV, you subtract calls 1 from 5, which makes 4.
+
+Roman numerals don't use four identical letters in a row. For example: You'd never exceed XXX, or 10+10+10, for the tens placement. Since we can't use four identical numerals in a row, 40 would not be XXXX but, rather, XL.
+
+Any time you see a line, that indicates the number should be multiplied by a thousand.For example: When V looks like V̅, that indicates 5 x 1000 = 5000.
+Remember to treat each part of the number separately (ones, tens, hundreds, etc.).
+
+For example: Even though 1999 is one fewer than 2000, you write MCMXCIX instead of MIM because you can’t skip place value.
--- src/roman.js
+++ src/roman.js
@@ -1,3 +1,24 @@
+var romanNumbers = ["I", "V", "X", "L", "C", "D", "M"];
+var romanEquivalents = [1, 5, 10, 50, 100, 500, 1000];
+
 export function Roman() {
   
+
+}
+
+function arrays(numberString) {
+  var numberArray = numberString.split();
+  return numberArray;
+}
+
+function numeral(numberArray) {
+  romanNumeralArray = []
+  numberArray.forEach(function(number) {
+    var numberIndex = romanEquivalents.indexOf(parseInt(number));
+    console.log("index " + numberIndex);
+    var romanNumeral = romanNumbers[numberIndex];
+    console.log("roman numeral " + romanNumeral);
+    romanNumeralArray.push(romanNumeral);
+  });
+  return romanNumeralArray
 }
